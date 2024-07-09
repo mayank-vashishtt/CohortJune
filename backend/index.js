f4ca51c3659a62a0eb61b7e7ef94a06307e4883f
mayank-vashishtt <mayankvashishtt@gmail.com>
2024-07-09 14:51:34 +0530

    hospital game backend

--- backend/index.js
+++ backend/index.js
@@ -1,28 +1,141 @@
-const express = require('express')
+// const express = require('express')
+// const app = express();
+// const port = process.env.PORT || 3000
+// const bodyParser = require('body-parser');
+
+// app.use(bodyParser.json());
+
+// //  create a todo app that lets user store todos on the server
+// app.get('/', function(req, res){
+//   res.send('<b>Hello World!<b>') // webpage/browser return html content , res.send(ans.toString()
+// })
+
+// app.get('/mayabk', function(req, res){
+//     res.json({  name: 'Madsya', age: 25, location: 'Nairobi' })
+// })
+
+// app.post('/mk', function(req, res){
+//     // console.log(req.body);
+//     const message = req.body.message;
+//     const message2 = req.query.message; // its like after url ?message=hello
+//     console.log(message);
+//     res.send({
+//         msg : "2 + 2 = 4"
+//     })
+
+// })
+
+
+// app.listen(port, () => {
+//     console.log(`Server is running on port ${port}`);
+// });
+
+// const express = require('express');
+// const app = express();
+
+// const port = process.env.PORT || 3000;
+
+
+// function sum(n){
+//     let ans = 0 ;
+//     for(let i = 0; i < n; i++){
+//         ans += i;
+//     }
+//     return ans ;
+// }
+
+// app.get('/', (req, res) => {
+//     const n = req.query.n;
+//     const ans = sum(n);
+
+//     res.send("and is " + ans.toString());
+
+
+// })
+
+
+
+
+// app.listen(port, () => {
+//     console.log(`Server is running on port ${port}`);
+// })
+
+const express = require('express'); 
 const app = express();
-const port = 3000
 const bodyParser = require('body-parser');
 
 app.use(bodyParser.json());
+const users = [{
+    name: 'Madsya',
+    Kidneys: [{
+        healthy: false
+    }]
+}];
 
-//  create a todo app that lets user store todos on the server
-app.get('/', function(req, res){
-  res.send('<b>Hello World!<b>') // webpage/browser return html content
-})
+app.get('/', (req, res) => {
+    const userKidneys = users[0].Kidneys; // Renamed to be more generic
+    // console.log(userKidneys);
+    const numberOfKidneys = userKidneys.length;
+    let numberOfHealthyKidneys = 0;
 
-app.get('/mayabk', function(req, res){
-    res.json({  name: 'Madsya', age: 25, location: 'Nairobi' })
-})
+    for (let i = 0; i < numberOfKidneys; i++) {
+        if (userKidneys[i].healthy) {
+            numberOfHealthyKidneys++;
+        }
+    }
 
-app.post('/mk', function(req, res){
-    console.log(req.body);
-    res.send({
-        msg : "2 + 2 = 4"
-    })
+    const numberOfUnhealthyKidneys = numberOfKidneys - numberOfHealthyKidneys;
 
-})
+    res.json({
+        // userKidneys,
+        numberOfHealthyKidneys,
+        numberOfUnhealthyKidneys
+    });
+});
+
+app.post('/', (req, res) => {
+    // body data 
+    const isHealthy = req.body.isHealthy;
+    users[0].Kidneys.push({
+        healthy: isHealthy
+    });
+
+    res.json({
+        message: 'Kidney added successfully',
+        kidneys: users[0].Kidneys
+    });
+});
 
 
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+app.put('/', (req, res) => {
+    for(let i =0 ; i< users[0].Kidneys.length; i++){
+        users[0].Kidneys[i].healthy = false;
+    }
+
+    res.json({});
+
+
+});
+
+app.delete('/', (req, res) => {
+    
+    const newKidneys = [] ; 
+    for(let i =0 ; i< users[0].Kidneys.length; i++){
+        if(users[0].Kidneys[i].healthy){
+            newKidneys.push(users[0].Kidneys[i]);
+        }
+    }
+
+    users[0].Kidneys = newKidneys;
+
+    res.json({
+        msg : "done"
+    });
+
+
+});
+
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
 });
