3ba2024d02e29221025c016eba54d2e05ce73f82
mayank-vashishtt <mayankvashishtt@gmail.com>
2024-08-04 07:14:23 +0530

    useState along with class, counter app

--- Reactort/src/App.jsx
+++ Reactort/src/App.jsx
@@ -3,18 +3,40 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+// function name should always start with capital letter 
 function App() {
-  const [count, setCount] = useState(10000)
+  const [count, setCount] = useState(0)
 
   return (
     <div>
       
         
-        <button onClick={() => setCount((count) => count + 1)}>
+        {/* <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
-        </button>
+
+          {/* use curly braces to write js in return of jsx  */}
+        {/* </button> */}  
+
+        <CustomButton count={count} setCount={setCount} />
+        <CustomButton count={count * 100} setCount={setCount} />
+
+
+        
         
     </div>
+  )
+
+
+}
+
+function CustomButton (props){
+  function onClickHandler(){
+    props.setCount(props.count + 1)
+  }
+  return (
+    <button onClick={onClickHandler}>
+      count is {props.count}
+    </button>
   )
 }
 
@@ -36,4 +58,33 @@ export default App
 // react generally converts into html, javascript and css
 
 
-// 
+// linkedin is owned by microsoft 
+
+//  website can be divied in states and components 
+
+// react is a library that is used to build user interfaces
+
+// dom - document object model
+
+
+// in react as developer we have to write short , but in html form its too large code 
+
+
+// alert() -- to show alert
+// .appencChild() -- to add element in dom
+// .removeChild() -- to remove element from dom
+// .innerHTML -- to change inner html
+// .innerText -- to change text
+// .style -- to change style
+// document.createElement() -- to create element
+// document.getElementById() -- to get
+
+
+// instagram is written in react 
+// create a react app -- npm create vite@latest 
+// then npm i 
+// then npm run dev
+
+// if you want to check how react code in converted into html and js -- use command  -- npm run build 
+
+// jsx -- a js file, inside which we can write both js and xml    
